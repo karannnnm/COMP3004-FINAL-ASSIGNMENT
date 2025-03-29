834969cf66f7dee6182750cde393c42838237550
gbassali <66041085+gbassali@users.noreply.github.com>
2025-03-28 23:08:41 -0400

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,45 @@
 #include "mainwindow.h"
+#include <QtCharts/QChartView>
+QT_CHARTS_USE_NAMESPACE
 #include "ui_mainwindow.h"
+#include <QtCharts/QLineSeries>
+#include <QtCharts/QChart>
+#include <QtCharts/QValueAxis>
+
+QT_CHARTS_USE_NAMESPACE
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QLineSeries *series = new QLineSeries();
+    series->append(0, 3.4);
+    series->append(1, 3.2);
+    series->append(2, 3.0);
+    series->append(3, 3.2);
+    series->append(4, 20);
+
+    QChart *chart = new QChart();
+    chart->legend()->hide();
+    chart->addSeries(series);
+
+    //chart axes
+    QValueAxis *axisY = new QValueAxis;
+    axisY->setRange(2, 22); // Set the range from 2 to 22
+    axisY->setTitleText("Glucose Level (mmol/L)");
+    chart->addAxis(axisY, Qt::AlignLeft);
+    series->attachAxis(axisY);
+
+    QValueAxis *axisX = new QValueAxis;
+    axisX->setTitleText("Time");
+    chart->addAxis(axisX, Qt::AlignBottom);
+    series->attachAxis(axisX);
+
+    chart->setTitle("CGM Data");
+
+    ui->chartView->setChart(chart);
+    ui->chartView->setRenderHint(QPainter::Antialiasing);
 }
 
 MainWindow::~MainWindow()
